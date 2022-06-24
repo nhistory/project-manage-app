2e8815a004ae63813172d3624f5232539d7b104f
nhistory <nhistory@naver.com>
2022-06-24 14:06:52 -0300

    Create Spinner component, clientMutation

--- client/src/components/Clients.jsx
+++ client/src/components/Clients.jsx
@@ -1,21 +1,12 @@
 import { gql, useQuery } from '@apollo/client';
 import ClientRow from './ClientRow';
-
-const GET_CLIENTS = gql`
-  query getClinets {
-    clients {
-      id
-      name
-      email
-      phone
-    }
-  }
-`;
+import Spinner from './Spinner';
+import { GET_CLIENTS } from '../queries/clientQueries';
 
 export default function Clients() {
   const { loading, error, data } = useQuery(GET_CLIENTS);
 
-  if (loading) return <p>loading...</p>;
+  if (loading) return <Spinner />;
   if (error) return <p>Something Went Wrong</p>;
 
   return (
