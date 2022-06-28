109f0a5d0d4a81d0ea5d2267037dda9a3bb522cc
nhistory <nhistory@naver.com>
2022-06-28 11:41:51 -0300

    Connect client delete with project delete

--- server/schema/schema.js
+++ server/schema/schema.js
@@ -103,6 +103,13 @@ const mutation = new GraphQLObjectType({
         id: { type: GraphQLNonNull(GraphQLID) },
       },
       resolve(parent, args) {
+        // if user delete client then associated project would be deleted
+        Project.find({ clientId: args.id }).then((projects) => {
+          projects.forEach((project) => {
+            project.remove();
+          });
+        });
+
         return Client.findByIdAndRemove(args.id);
       },
     },
