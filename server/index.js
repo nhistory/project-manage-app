55cc9fb429273461c68ccb1ef5f8f208809babf3
nhistory <nhistory@naver.com>
2022-06-29 21:51:58 -0300

    Preparation for deploy with heroku

--- server/index.js
+++ server/index.js
@@ -1,3 +1,4 @@
+const path = require('path');
 const express = require('express');
 const colors = require('colors');
 const cors = require('cors');
@@ -21,5 +22,18 @@ app.use(
     graphiql: process.env.NODE_ENV === 'development',
   })
 );
+
+// Serve frontend
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, '../client/build')));
+
+  app.get('*', (req, res) =>
+    res.sendFile(
+      path.resolve(__dirname, '../', 'client', 'build', 'index.html')
+    )
+  );
+} else {
+  app.get('/', (req, res) => res.send('Please set to production'));
+}
 
 app.listen(port, console.log(`Server running on port ${port}`));
